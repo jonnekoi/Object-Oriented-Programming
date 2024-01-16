8a078c8a8f0c1a39ce28481e1554308cbad3aee0
jonne <jonnekoivisto00@gmail.com>
2024-01-16 10:34:10 +0200

    commit

---
+++ src/Module01/Task02.java
@@ -0,0 +1,11 @@
+package Module01;
+
+public class Task02 {
+    public static void main(String [] rgs) {
+        System.out.println("   *");
+        System.out.println("  *** ");
+        System.out.println(" ***** ");
+        System.out.println("*******");
+
+    }
+}

8fa299f4d4778a6949debd28ce2947ca99f2b6f9
jonne <jonnekoivisto00@gmail.com>
2024-01-18 15:27:45 +0200

    commit

--- src/Module1_3/controlStructures.java
+++ src/Module1_3/controlStructures.java
@@ -1,9 +1,10 @@
 package Module1_3;
 
 import java.util.Scanner;
+import java.util.Random;
 
 public class controlStructures {
-    public static void main(String[] args) {
+    /*public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
 
         System.out.println("Give a number: ");
@@ -11,6 +12,16 @@ public class controlStructures {
 
         for (int i = number; i <= 50; i++) {
             System.out.println(i);
+        }
+    }
+}
+*/
+
+    public static void main(String[] args){
+        Random rand = new Random();
+        for (int i = 0; i < 30; i++) {
+            int n = rand.nextInt(50);
+            System.out.println(n);
         }
     }
 }

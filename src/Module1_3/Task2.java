1311327f40e76616faa4c0bdd9fb6ab7d847fabb
jonne <jonnekoivisto00@gmail.com>
2024-01-19 00:54:53 +0200

    commit

--- src/Module1_3/Task2.java
+++ src/Module1_3/Task2.java
@@ -1,4 +1,16 @@
 package Module1_3;
 
+import java.util.Scanner;
+
 public class Task2 {
+    public static void main(String[] args){
+        Scanner reader = new Scanner(System.in);
+
+        System.out.print("Enter a binary to convert: ");
+        String inputNumber = reader.nextLine();
+
+        int convertDecimal = Integer.parseInt(inputNumber,2);
+
+        System.out.println("Decimal value is: " + convertDecimal);
+    }
 }

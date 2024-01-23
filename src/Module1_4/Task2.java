55d3e2e8d98b79786ce392e73515aa166337d6a6
jonne <jonnekoivisto00@gmail.com>
2024-01-23 13:00:35 +0200

    commit

--- src/Module1_4/Task2.java
+++ src/Module1_4/Task2.java
@@ -18,10 +18,24 @@ public class Task2 {
             numbers[i] = reader.nextInt();
         }
 
-                for (int number : numbers) {
-                    System.out.println(number);
+        int maxSum = Integer.MIN_VALUE;
+        int startIndex = 0, endIndex = 0;
+        for (int i = 0; i < arraySize; i++) {
+            int currentSum = 0;
+            for (int j = i; j < arraySize; j++) {
+                currentSum += numbers[j];
+                if (currentSum > maxSum) {
+                    maxSum = currentSum;
+                    startIndex = i;
+                    endIndex = j;
                 }
             }
+        }
+        System.out.println("Maximum sum: " + maxSum);
+        System.out.println("Integers: " + (startIndex + 1) + "-" + (endIndex + 1));
+
+
+    }
 }
 
 

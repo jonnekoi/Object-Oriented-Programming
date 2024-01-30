cf0864351248c3f1b3374cb42775168f7eeaf254
jonne <jonnekoivisto00@gmail.com>
2024-01-30 10:41:04 +0200

    added setRating and addReview

--- src/Module2_3/Book.java
+++ src/Module2_3/Book.java
@@ -1,13 +1,18 @@
 package Module2_3;
 
+import java.util.ArrayList;
+
 public class Book {
     private String title;
     private String author;
     private int publicationYear;
+    private double rating;
+    private ArrayList<String> reviews;
     public Book(String title, String author, int publicationYear){
         this.title = title;
         this.author = author;
         this.publicationYear = publicationYear;
+        this.reviews = new ArrayList<>();
     }
 
     public String getTitle(){
@@ -18,5 +23,13 @@ public class Book {
     }
     public int getPublicationYear(){
         return publicationYear;
+    }
+
+    public void setRating(double rating){
+        this.rating = rating;
+    }
+
+    public void addReview(String review){
+        reviews.add(review);
     }
 }

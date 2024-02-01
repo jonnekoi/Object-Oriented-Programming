120a664d193e137a12daf9a830e8cd7e961af2ce
jonne <jonnekoivisto00@gmail.com>
2024-02-01 09:40:02 +0200

    commit

---
+++ src/Module3_1/Car.java
@@ -0,0 +1,44 @@
+package Module3_1;
+
+public class Car {
+    private float speed;
+    private float gasolineLevel;
+    private float tankCapacity;
+    private float topSpeed;
+    private String typeName;
+
+
+    public Car(String typeName) {
+        speed = 0;
+        gasolineLevel = 0;
+        this.tankCapacity = 100;
+        this.topSpeed = 100;
+        this.typeName = typeName;
+    }
+
+    public void accelerate() {
+        if (gasolineLevel > 0)
+            speed += 10;
+        else
+            speed = 0;
+    }
+    void decelerate(int amount) {
+        if (gasolineLevel > 0) {
+            if (amount > 0)
+                speed = Math.max(0, speed - topSpeed);
+        } else
+            speed = 0;
+    }
+    float getSpeed() {
+        return speed;
+    }
+    String getTypeName() {
+        return typeName;
+    }
+    void fillTank() {
+        gasolineLevel = tankCapacity;
+    }
+    float getGasolineLevel() {
+        return gasolineLevel;
+    }
+}

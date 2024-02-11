94bc4067648a3ea1f67a160acd9f20f7a591464f
jonnek <jonnekoivisto00@gmail.com>
2024-02-11 11:24:18 +0200

    bus

--- src/Module3_2/AbstractCar.java
+++
@@ -1,9 +0,0 @@
-package Module3_2;
-
-public class AbstractCar extends AbstractVehicle{
-    private String color;
-    public AbstractCar(String fuel, String type, String color){
-        super(fuel, type);
-        this.color = color;
-    }
-}

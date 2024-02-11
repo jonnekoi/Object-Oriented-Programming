a291531d295d8264f2a819c15507dbcc9289d1f2
jonnek <jonnekoivisto00@gmail.com>
2024-02-11 11:31:47 +0200

    BankAccount.java

---
+++ src/Module3_3/BankAccount.java
@@ -0,0 +1,48 @@
+package Module3_3;
+
+public class BankAccount {
+    private int accountNumber;
+    private double balance;
+    private static int totalAccounts;
+
+    public BankAccount(double balance){
+        this.balance = balance;
+        totalAccounts++;
+        accountNumber = totalAccounts;
+    }
+
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public void withdraw(double amount) {
+        balance -= amount;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public static int getTotalAccounts() {
+        return totalAccounts;
+    }
+
+
+    public static void main(String[] args) {
+        BankAccount account1 = new BankAccount(1000);
+        BankAccount account2 = new BankAccount(2000);
+
+        account1.deposit(500);
+        account2.withdraw(800);
+
+        System.out.println("Account " + account1.getAccountNumber() + " balance: " + account1.getBalance());
+        System.out.println("Account " + account2.getAccountNumber() + " balance: " + account2.getBalance());
+
+        System.out.println("Total number of accounts: " + BankAccount.getTotalAccounts());
+    }
+}
